956da36537f52390128833bee1589ec71da12bd1
HeitorADL <114448715+HeitorADL@users.noreply.github.com>
2022-10-24 13:00:02 -0400

    Add files via upload

---
+++ Atividade4-Lógica de programação.js
@@ -0,0 +1,36 @@
+//Inicio
+console.log("Informe seu nome")
+let nome = "Heitor Lima"
+if(nome.length<2){
+    console.log(nome)
+    console.log("Nome inválido")
+}
+else{
+    console.log(nome)
+    console.log("Informe sua idade")
+}
+let idade=19
+if (idade<=18) {
+    console.log(idade)
+    console.log("Idade não compatível")
+} else {
+    console.log(idade)
+    console.log("Digite a data de comparecimento")
+}
+let dia=12
+let mes=05
+if (dia>31||dia<1||mes>12||mes<1) {
+    console("Digite uma data válida")
+} else {
+    console.log(dia,"/",mes)
+    console.log("Selecione o tipo de ingresso")
+}
+let ingresso="Meia-entrada"
+if (ingresso="Inteira") {
+    console.log("Seu cadastro foi concluído")
+} else {
+        console.log("Seu cadastro foi concluído")
+}
+// Fim
+
+
